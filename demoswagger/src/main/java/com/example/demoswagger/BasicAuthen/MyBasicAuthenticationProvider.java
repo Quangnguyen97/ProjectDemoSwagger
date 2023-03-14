1ae420ac9231bf31fec363127d3f1535df5a57af
Nguyễn Huy Quang <35794747+Quangnguyen97@users.noreply.github.com>
2023-03-14 22:56:59 +0700

    .

--- demoswagger/src/main/java/com/example/demoswagger/BasicAuthen/MyBasicAuthenticationProvider.java
+++ demoswagger/src/main/java/com/example/demoswagger/BasicAuthen/MyBasicAuthenticationProvider.java
@@ -1,8 +1,10 @@
 package com.example.demoswagger.BasicAuthen;
 
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -10,6 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 @Component
@@ -20,20 +23,30 @@ public class MyBasicAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication auth) throws AuthenticationException {
-        String username = auth.getName();
-        String password = auth.getCredentials()
-                .toString();
+        try {
+            String username = auth.getName();
+            String password = auth.getCredentials()
+                    .toString();
 
-        MyBasicAuthenticationLogin myBasicAuthenticationLogin = new MyBasicAuthenticationLogin();
+            String sql = "SELECT TOP 1 MaNguoiDung, MatKhau FROM COM_systbl_NguoiDung " +
+                    "WHERE MaNguoiDung=N'" + username + "' AND MatKhau=N'" + password + "' AND KhongHienHoatHT=0";
+            List<MyBasicAuthenticationLogin> ListLogin = jdbcTemplate.query(sql,
+                    BeanPropertyRowMapper.newInstance(MyBasicAuthenticationLogin.class));
 
-        String sql = "EXEC sp_GETTBL_ForAndroid_BanHang_HangHoa " + username + ", " + password + "";
-        myBasicAuthenticationLogin = jdbcTemplate.queryForObject(sql, new Object[] { username },
-                new MyBasicAuthenticationLoginRowMapper());
-
-        if ("externaluser".equals(username) && "pass".equals(password)) {
+            if (ListLogin.isEmpty()) {
+                throw new BadCredentialsException("Login information " + HttpStatus.NOT_FOUND.getReasonPhrase());
+            }
+            MyBasicAuthenticationLogin Login = ListLogin.stream()
+                    .filter(myBasicAuthenticationLogin -> username.equals(myBasicAuthenticationLogin.getMaNguoiDung()))
+                    .filter(myBasicAuthenticationLogin -> password.equals(myBasicAuthenticationLogin.getMatKhau()))
+                    .findAny()
+                    .orElse(null);
+            if (Login == null) {
+                throw new BadCredentialsException("External system authentication failed");
+            }
             return new UsernamePasswordAuthenticationToken(username, password, Collections.emptyList());
-        } else {
-            throw new BadCredentialsException("External system authentication failed");
+        } catch (Exception e) {
+            throw new BadCredentialsException(e.getMessage());
         }
     }
 
