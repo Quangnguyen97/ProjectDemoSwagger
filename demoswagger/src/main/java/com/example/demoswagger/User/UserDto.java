3cf9590b9eb955bb620f3262a3ef1104cb7be0dd
Nguyễn Huy Quang <nhq14081997@gmail.com>
2023-03-17 14:40:43 +0700

    .

--- demoswagger/src/main/java/com/example/demoswagger/User/UserDto.java
+++ demoswagger/src/main/java/com/example/demoswagger/User/UserDto.java
@@ -2,6 +2,8 @@ package com.example.demoswagger.User;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
@@ -10,19 +12,23 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDto {
+    @ApiModelProperty(notes = "Mã định danh", example = "1")
     @JsonProperty("userId")
     private long userId;
 
+    @ApiModelProperty(notes = "Tên đầy đủ", example = "Nguyễn Văn A")
     @NotNull
     @NotEmpty(message = "fullName must not be empty")
     @JsonProperty("fullName")
     private String fullName;
 
+    @ApiModelProperty(notes = "Mật khẩu", example = "123456")
     @NotNull
     @NotEmpty(message = "password must not be empty")
     @JsonProperty("password")
     private String password;
 
+    @ApiModelProperty(notes = "Mã điện tử", example = "Tokenabc123")
     @NotNull
     @NotEmpty(message = "notificationToken must not be empty")
     @JsonProperty("notificationToken")
